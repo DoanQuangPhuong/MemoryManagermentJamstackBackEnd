ebd6311c9d5143ff173cbbc2b4442952f7166648
DoanQuangPhuong <quangphuong080603@gmail.com>
2024-10-11 17:49:15 +0700

    update to connect vercel

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-const port = 5000;
+// const port =  5000;
+const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
@@ -104,7 +105,11 @@ app.post('/allocate', (req, res) => {
   res.json({ allocations, fragmentations, remainingBlocks });
 });
 
-
 app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
+  console.log(`Server is running at port ${port}`);
 });
+
+
+// app.listen(port, () => {
+//   console.log(`Server is running at http://localhost:${port}`);
+// });
